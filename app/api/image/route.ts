05c97906037562499dfb4559ed3270a8fac3d5b2
Bassey Duke <bassey.bd@gmail.com>
2025-01-03 15:39:23 -0500

    added image generation components via openai

---
+++ app/api/image/route.ts
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  organization: "org-iBkLWvwZsnQ8pz850IaIlJXh",
+  project: "proj_i1TxD3Y2jhkVlIl2NvvLYI7I",
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = await auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = "512x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!prompt) return new NextResponse("Prompt is Required", { status: 400 });
+
+    if (!amount) return new NextResponse("Amount is Required", { status: 400 });
+
+    if (!resolution)
+      return new NextResponse("Resolution is Required", { status: 400 });
+
+    // Make the OpenAI call
+    const response = await openai.images.generate({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error("[IMAGE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
