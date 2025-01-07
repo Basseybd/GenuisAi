820b99d6884f9d0cf3700cc750629511382c3f8e
Bassey Duke <bassey.bd@gmail.com>
2025-01-06 20:45:57 -0500

    added an eslint exception

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
 const prismadb =
   globalThis.prisma ||
   new PrismaClient({
-    log: ["query", "info", "warn", "error"], // Helpful for debugging in development
+    log: ["query", "info", "warn", "error"],
   });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
